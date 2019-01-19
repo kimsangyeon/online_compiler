4ddef99b43f47a4c84664cd0ac544a2afedccf58
Kim Sangyeon <dho113@naver.com>
2019-01-19 10:33:23 +0900

    task/arrange-server-code-file
    - move controller.js, router.js into server folder

--- router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const controller = require('./server/controller');
-const router = express.Router();
-
-router.get('/', controller.index);
-router.post('/compile', controller.compile);
-
-module.exports = router;
