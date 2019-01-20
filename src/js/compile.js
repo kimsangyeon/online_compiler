1e54ef3b83316e0d65da1f43a41a5806d30f5650
Kim Sangyeon <dho113@naver.com>
2019-01-20 19:52:22 +0900

    task/modify-index-html
    - add compile output, time

--- src/js/compile.js
+++ src/js/compile.js
@@ -8,13 +8,16 @@ window.onload = () => {
     codeMirror.setValue("// Your code here!\nfunction fn() { \n    return;\n}");
 
     const elBtn = document.getElementById('compile-btn');
+    const elCompileOutput = document.getElementById('compile-output');
+    const elCompileTime = document.getElementById('compile-time');
     elBtn.onclick = () => {
         $.ajax({
             url: `${location.protocol}//${location.hostname}:${location.port}/compile`,
             type: "POST",
             data: {'code': codeMirror.getValue()},
             success: (data) => {
-                console.log(data);
+                elCompileOutput.textContent = data.stdout;
+                elCompileTime.textContent = data.time;
             }
         });
     }
