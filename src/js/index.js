33915d960f5b87e41995aac1ef9d1f6218a0ab52
Kim Sangyeon <dho113@naver.com>
2019-02-04 12:52:48 +0900

    bug/error-index-js
    - modify get editor code

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ const {LANGUAGE, CODE_DEFAULT} = consts;
 
 window.onload = () => {
     const compiler = new Compiler('codesnippet_editable' , LANGUAGE.JAVASCRIPT, CODE_DEFAULT.JAVASCRIPT);
+    const editor = compiler.getEditor();
     const elSelect = document.getElementById('compile-select');
     const elBtn = document.getElementById('compile-btn');
     const elCompileOutput = document.getElementById('compile-output');
@@ -13,7 +14,7 @@ window.onload = () => {
         $.ajax({
             url: `${location.protocol}//${location.hostname}:${location.port}/compile`,
             type: "POST",
-            data: {'language': compiler.getLanguage(), 'code': compiler.getForm().getValue()},
+            data: {'language': compiler.getLanguage(), 'code': editor.getForm().getValue()},
             success: (data) => {
                 elCompileOutput.textContent = data.stdout;
                 elCompileTime.textContent = data.time;
