b2f57a7288b4b9102d8cc3134033832ddf2723c4
Kim Sangyeon <dho113@naver.com>
2019-02-08 00:12:50 +0900

    task/Editor-js-remark
    - add Editor.js remark

--- src/js/Editor.js
+++ src/js/Editor.js
@@ -9,10 +9,18 @@ class Editor {
         });
     }
 
+    /**
+     * Get Editor form object
+     * @return {Object} form
+     **/
     getForm() {
         return this.form;
     }
 
+    /**
+     * Editor form set value
+     * @param {String} value
+     **/
     setValue(value) {
         this.form.setValue(value);
     }
