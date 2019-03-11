4814391a4e221ed1b601f1d9070ffdbb518bf866
Kim Sangyeon <dho113@naver.com>
2019-03-12 00:41:34 +0900

    task/arrange indent
    - algorithm.js, contsts.js indent tab -> space

--- server/consts/algorithm.js
+++ server/consts/algorithm.js
@@ -4,27 +4,27 @@
 const algorithm = {
     none: [],
     factorial: [{
-    	args: "1",
-    	result: "1"
+        args: "1",
+        result: "1"
     },{
-    	args: "2",
-    	result: "2"
+        args: "2",
+        result: "2"
     },{
-    	args: "3",
-    	result: "6"
+        args: "3",
+        result: "6"
     },{
-    	args: "4",
-    	result: "24"
+        args: "4",
+        result: "24"
     },{
-    	args: "5",
-    	result: "120"
+        args: "5",
+        result: "120"
     }],
     gcd: [{
-    	args: "12 30",
-    	result: "6"
+        args: "12 30",
+        result: "6"
     },{
-    	args: "8 40",
-    	result: "8"
+        args: "8 40",
+        result: "8"
     }]
 };
 
--- src/js/consts/consts.js
+++ src/js/consts/consts.js
@@ -2,34 +2,34 @@
  * consts
  */
 const consts = {
-	LANGUAGE: {
-		JAVASCRIPT: 'javascript',
-		PYTHON: 'python',
-		JAVA: 'java'
-	},
-	MODE: {
-		JAVASCRIPT: 'javascript',
-		PYTHON: 'python',
-		JAVA: 'text/x-java'
-	},
-	CODE: {
-		javascript: {
-			none: `// Your code here!\nfunction fn(n) { \n    return n;\n}`,
-			factorial: `// Your code here!\nfunction fn(n) { \n    return n;\n}`,
-			gcd: `// Your code here!\nfunction fn(a, b) { \n    return;\n}`
-		},
-		python: {
-			none: `# Your code here!\ndef fn(n):\n    return n\n`
-		},
-		java: {
-			none: `// Your code here!\nstatic class Prac\n{\n    public String fn(n) {\n        return "Hello, World!";\n    }\n}`
-		}
-	},
-	ALGORITHM: {
-		NONE: 'none',
-		FACTORIAL: 'factorial',
-		GCD: 'gcd'
-	}
+    LANGUAGE: {
+        JAVASCRIPT: 'javascript',
+        PYTHON: 'python',
+        JAVA: 'java'
+    },
+    MODE: {
+        JAVASCRIPT: 'javascript',
+        PYTHON: 'python',
+        JAVA: 'text/x-java'
+    },
+    CODE: {
+        javascript: {
+            none: `// Your code here!\nfunction fn(n) { \n    return n;\n}`,
+            factorial: `// Your code here!\nfunction fn(n) { \n    return n;\n}`,
+            gcd: `// Your code here!\nfunction fn(a, b) { \n    return;\n}`
+        },
+        python: {
+            none: `# Your code here!\ndef fn(n):\n    return n\n`
+        },
+        java: {
+            none: `// Your code here!\nstatic class Prac\n{\n    public String fn(n) {\n        return "Hello, World!";\n    }\n}`
+        }
+    },
+    ALGORITHM: {
+        NONE: 'none',
+        FACTORIAL: 'factorial',
+        GCD: 'gcd'
+    }
 };
 
 export default consts;
