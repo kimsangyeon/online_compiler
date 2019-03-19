b6305404ceecd4e4b540aca8c608b1207d17471b
Kim Sangyeon <dho113@naver.com>
2019-03-19 13:53:11 +0900

    task/canvas class
    - index_canvas.js: import Canvas, import consts, modify onload code

--- src/js/index_canvas.js
+++ src/js/index_canvas.js
@@ -1,10 +1,9 @@
-window.onload = () => {
-    const codemirror = CodeMirror.fromTextArea(document.getElementById('codesnippet-editable'), {
-        mode: 'javascript',
-        lineNumbers: true
-    });
-    codemirror.setValue("var ctx = document.getElementById('canvas').getContext('2d');");
+import Canvas from './Canvas';
+import consts from './consts/consts';
 
+const {LANGUAGE, MODE, CODE, ALGORITHM} = consts;
+
+window.onload = () => {
     const elCanvas = document.getElementById('canvas');
     const elCanvasEx = document.getElementById('canvas-ex');
     const context = elCanvas.getContext('2d');
@@ -12,10 +11,12 @@ window.onload = () => {
     const elDrawResult = document.getElementById('draw-result');
     const elDrawBtn = document.getElementById('draw-btn');
 
+    let canvas = new Canvas('codesnippet-editable' , LANGUAGE.JAVASCRIPT, MODE.JAVASCRIPT, CODE[LANGUAGE.JAVASCRIPT][ALGORITHM.CANVAS]);
+
     elDrawBtn.onclick = () => {
         context.clearRect(0, 0, elCanvas.offsetWidth, elCanvas.offsetHeight);
 
-        const code = codemirror.getValue();
+        const code = canvas.getEditor().getForm().getValue();
         const drawFunction = new Function(code);
         drawFunction();
 
@@ -32,9 +33,9 @@ window.onload = () => {
  * init canvas draw (fillRect)
  * @param {HTMLElement} canvas
  */
-function initDraw(canvas) {
-    if (canvas.getContext) {
-        const ctx = canvas.getContext('2d');
+function initDraw(elCanvas) {
+    if (elCanvas.getContext) {
+        const ctx = elCanvas.getContext('2d');
 
         ctx.fillRect(25, 25, 100, 100);
         ctx.clearRect(45, 45, 60, 60);
