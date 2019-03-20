426781e97bd0c2ae4e90ea1d336bf867d5cb3c8c
Kim Sangyeon <dho113@naver.com>
2019-03-20 22:48:34 +0900

    task/add_canvas_question
    - add question.js consts file

---
+++ src/js/consts/questions.js
@@ -0,0 +1,10 @@
+/**
+ * questions
+ */
+const questions = {
+    CANVAS: {
+        FILLRECT: `var ctx = document.getElementById('canvas').getContext('2d');<br>ctx.    (25, 25, 100, 100);<br>ctx.    (45, 45, 60, 60);<br>ctx.    (50, 50, 50, 50);`
+    }
+};
+
+export default questions;
