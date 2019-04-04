e33dd08e95c70cdc850004b7033919bc33f39fd0
Kim Sangyeon <dho113@naver.com>
2019-04-04 13:49:07 +0900

    task/create table component
    - cerate Table component

---
+++ src/js/components/content/Table.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Table extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        
+        return (
+            // table
+            <table></table>
+        );
+    }
+}
+
+export default Button;
